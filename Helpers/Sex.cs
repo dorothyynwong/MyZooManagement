0f4f10e9ff0b9bab29624ec192e8bba841dab84a
dorothywyn <dorothywyn@users.noreply.github.com>
2024-08-23 17:31:50 +0100

    Make Swagger able to show Male and Female for enum Sex

--- Helpers/Sex.cs
+++ Helpers/Sex.cs
@@ -1,8 +1,12 @@
- namespace ZooManagement.Helpers
+using System.Runtime.Serialization;
+
+namespace ZooManagement.Helpers
  {
     public enum Sex
     {
-        Male,
-        Female
+        [EnumMember(Value = "Male")]
+        Male = 0,
+        [EnumMember(Value = "Female")]
+        Female = 1
     }
  }  
