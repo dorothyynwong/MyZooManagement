5723cb066502f6a81b240576c1677583d2793218
Dorothy Wong <dorothyynwong@gmail.com>
2024-08-30 13:22:42 +0100

    setup ZooKeeperFeedModel

--- Models/Response/ZooKeeperFeedModel.cs
+++ Models/Response/ZooKeeperFeedModel.cs
@@ -1,5 +1,6 @@
 using ZooManagement.Models.Database;
 using ZooManagement.Models.Request;
+using ZooManagement.Repositories;
 
 namespace ZooManagement.Models.Response
 {  
@@ -8,24 +9,37 @@ namespace ZooManagement.Models.Response
     {
         public FeedZooKeeperModel(ZooKeeper zooKeeper) : base(zooKeeper) 
         { 
-
+            Enclosures = zooKeeper.Enclosures
+                            .Select(e => new FeedEnclosureModel(e));
         }
+
+        public IEnumerable<FeedEnclosureModel> Enclosures { get; }
     }
 
     public class FeedEnclosureModel : EnclosureResponse
     {
         public FeedEnclosureModel(Enclosure enclosure) : base(enclosure) 
         {
-
+            Animals = enclosure.Animals
+                        .Select(e => new FeedAnimalModel(e));
+            ZooKeepers = enclosure.ZooKeepers
+                        .Select(e => new FeedZooKeeperModel(e));
         }
+
+        public IEnumerable<FeedAnimalModel> Animals {get;}
+        public IEnumerable<FeedZooKeeperModel> ZooKeepers {get;}
     }
 
     public class FeedEnclosureZooKeeperModel : EnclosureZooKeeperResponse
     {
         public FeedEnclosureZooKeeperModel(EnclosureZooKeeper enclosureZooKeeper) : base(enclosureZooKeeper) 
         { 
-            
+            Enclosure = new FeedEnclosureModel(enclosureZooKeeper.Enclosure);
+            ZooKeeper = new FeedZooKeeperModel(enclosureZooKeeper.ZooKeeper);
         }
+
+        public FeedEnclosureModel Enclosure { get; }
+        public FeedZooKeeperModel ZooKeeper { get; }
     }
 
 
